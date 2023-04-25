507d9a66c27d698d002be7c88246565db0cf77b0
avonavia <121552637+avonavia@users.noreply.github.com>
2023-04-25 13:55:24 +0300

    Add files via upload

---
+++ BigFIleReaderWriter16042023/FileReaderWriter.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System;
+
+namespace BigFIleReaderWriter16042023
+{
+    public class FileReaderWriter
+    {
+        public static void ReadWriteFile(string readFilePath, string writeFilePath)
+        {
+            try
+            {
+                var startTime = System.Diagnostics.Stopwatch.StartNew();
+                StreamReader reader = new StreamReader(File.OpenRead(readFilePath));
+                StreamWriter writer = new StreamWriter(File.OpenWrite(writeFilePath));
+                long bytesTotal = 0;
+
+                while (reader.Peek() >= 0)
+                {
+                    char[] readBuffer = new char[4096];
+                    bytesTotal += reader.ReadBlock(readBuffer, 0, readBuffer.Length);
+                    writer.Write(readBuffer);
+                }
+                startTime.Stop();
+                var resultTime = startTime.Elapsed;
+                string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}", resultTime.Minutes, resultTime.Seconds, resultTime.Milliseconds);
+                reader.Close();
+
+                Console.WriteLine($"Finished in {elapsedTime}");
+                Console.WriteLine($"Read {bytesTotal} bytes from file: {readFilePath}");
+                Console.WriteLine($"Wrote {new FileInfo(writeFilePath).Length} bytes to file: {writeFilePath}");
+            }
+            catch(Exception Error)
+            {
+                Console.WriteLine($"Error: {Error.Message}");
+            }
+        }
+    }
+}
+
---
+++ BigFIleReaderWriter16042023/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BigFIleReaderWriter16042023
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string path0 = @"..\test1.txt"; //2 mb | ~time: 0.27 sec 
+            const string path1 = @"C:\Program Files (x86)\Steam\steamapps\common\Super Auto Pets\UnityPlayer.dll"; //27~ mb | ~time: 0.909 sec
+            const string path2 = @"C:\Games\BloonsTD6\TD6_batch-35.2-release.6313_release.zip"; //2~ gb | ~time: 50.503 sec
+            const string path3 = @"C:\Users\nyanp\Downloads\Downlds\ru_windows_10_pro_64bit.iso"; //4~ gb | ~time: 2.52 min
+            const string path4 = @"C:\Users\nyanp\VirtualBox VMs\Windows 10\Windows 10.vdi"; //10~ gb | ~time: 4.47 min
+
+            const string writingPath = @"C:\Users\nyanp\Desktop\test.txt";
+
+            FileReaderWriter.ReadWriteFile(path1, writingPath);
+        }
+    }
+}
